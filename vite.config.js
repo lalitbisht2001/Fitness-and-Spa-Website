a6007cbdff6a00b2875afde5b9ab3cef57f9084a
lalitbisht2001 <lalitbisht2025@gmail.com>
2024-08-30 09:34:02 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -5,22 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/Fitness-and-Spa-Website/',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000
-  },
-  css: {
-    postcss: {
-      plugins: [
-        postcssNested,
-        postcssPresetEnv,
-      ]
-    }
-  }
 })
